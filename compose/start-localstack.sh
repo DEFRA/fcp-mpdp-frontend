3c870a61da82af767fa9941e741f7743ebeb873c
Rana Salem <ranatasalem@gmail.com>
2025-07-01 16:34:53 +0100

    add s3 bucket creation for perf tests

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -5,7 +5,9 @@ export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 
 # S3 buckets
-# aws --endpoint-url=http://localhost:4566 s3 mb s3://my-bucket
+echo "Creating S3 bucket: fcp-mpdp-frontend-perf-tests"
+aws --endpoint-url=http://localhost:4566 s3 mb s3://fcp-mpdp-frontend-perf-tests
+echo "S3 bucket fcp-mpdp-frontend-perf-tests successfully created"
 
 # SQS queues
 # aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name my-queue
