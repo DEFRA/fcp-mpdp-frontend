e55e5f231edaa7c82c35333102d7bec8b31d6d40
Rana Salem <ranatasalem@gmail.com>
2025-07-16 09:36:06 +0100

    update id to be kebab case in partial

--- src/server/service-start/controller.js
+++ src/server/service-start/controller.js
@@ -1,11 +1,8 @@
-import { getRelatedContentLinks } from '../../config/related-content.js'
-
 export const serviceStartController = {
   handler(_request, h) {
     return h.view('service-start/index', {
       pageTitle: 'Find farm and land payment data',
-      heading: 'Find farm and land payment data',
-      relatedContentData: getRelatedContentLinks('service-start')
+      heading: 'Find farm and land payment data'
     })
   }
 }
