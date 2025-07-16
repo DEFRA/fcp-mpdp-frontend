c01e49fb1a57d973dd29adbf92f1b8eb83f74bb7
Rana Salem <ranatasalem@gmail.com>
2025-07-16 11:57:38 +0100

    remove pageTitle

--- src/server/start/controller.js
+++ src/server/start/controller.js
@@ -1,8 +1,5 @@
 export const startController = {
   handler(_request, h) {
-    return h.view('start/index', {
-      pageTitle: 'Find farm and land payment data',
-      heading: 'Find farm and land payment data'
-    })
+    return h.view('start/index')
   }
 }
