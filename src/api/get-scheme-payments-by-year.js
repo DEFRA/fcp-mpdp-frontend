8c49ebeafcca9ac8734c1b42ab31a9742a33b5a4
Rana Salem <ranatasalem@gmail.com>
2025-08-12 11:48:08 +0100

    restore temp changes to api

--- src/api/get-scheme-payments-by-year.js
+++ src/api/get-scheme-payments-by-year.js
@@ -1,10 +1,5 @@
-// import { getUrlParams } from './get-url-params.js'
-// import { get } from './get.js'
-import Wreck from '@hapi/wreck'
-import { config } from '../config/config.js'
-import { createLogger } from '../common/helpers/logging/logger.js'
-
-const logger = createLogger()
+import { getUrlParams } from './get-url-params.js'
+import { get } from './get.js'
 
 export async function getSchemePaymentsByYear () {
   const url = getUrlParams('summary')
@@ -15,21 +10,4 @@ export async function getSchemePaymentsByYear () {
   }
 
   return JSON.parse(response.payload)
-}
-
-function getUrlParams (page, params = {}) {
-  if (Object.keys(params).length === 0) {
-    return `/${page}`
-  }
-
-  return `/${page}?${new URLSearchParams(params).toString()}`
-}
-
-async function get (url) {
-  try {
-    return (await Wreck.get(`${config.get('backend.endpoint')}${config.get('backend.path')}${url}`))
-  } catch (error) {
-    logger.error(`Encountered error while calling the backend with url: ${config.backendEndpoint}${url}}`, error)
-    return null
-  }
 }
