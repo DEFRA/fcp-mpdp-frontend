1299da2152f3118660d020f9f7a69450d2310d0a
Jayne Elliott <Jayne.elliott@defra.gov.uk>
2025-08-19 15:45:15 +0100

    Fix quote style in phase banner expectation test

--- test/utils/phase-banner-expect.js
+++ test/utils/phase-banner-expect.js
@@ -6,5 +6,5 @@ export function expectPhaseBanner ($) {
   expect(phaseBanner.length).toEqual(1)
   expect($('.govuk-phase-banner__content__tag').text()).toMatch('Beta')
   expect($('.govuk-phase-banner__text').text()).toMatch('This is a new service. Help us improve it and give your feedback (opens in new tab).')
-  expect(link.attr("href")).toMatch("https://defragroup.eu.qualtrics.com/jfe/form/SV_1FcBVO6IMkfHmbs")
+  expect(link.attr('href')).toMatch('https://defragroup.eu.qualtrics.com/jfe/form/SV_1FcBVO6IMkfHmbs')
 }
