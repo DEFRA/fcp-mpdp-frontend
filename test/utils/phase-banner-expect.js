189c1d19026059c26b1d9db0c1f4381c4bc6b7e2
Jayne Elliott <Jayne.elliott@defra.gov.uk>
2025-08-19 15:46:15 +0100

    Add a blank line for improved readability in phase banner expectation test

--- test/utils/phase-banner-expect.js
+++ test/utils/phase-banner-expect.js
@@ -3,6 +3,7 @@ import { expect } from 'vitest'
 export function expectPhaseBanner ($) {
   const phaseBanner = $('.govuk-phase-banner')
   const link = phaseBanner.find('.govuk-link')
+
   expect(phaseBanner.length).toEqual(1)
   expect($('.govuk-phase-banner__content__tag').text()).toMatch('Beta')
   expect($('.govuk-phase-banner__text').text()).toMatch('This is a new service. Help us improve it and give your feedback (opens in new tab).')
