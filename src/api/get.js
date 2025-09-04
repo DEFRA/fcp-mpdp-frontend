d08ae195160cb805791b94d162852ecc9afe2d49
Rana Salem <ranatasalem@gmail.com>
2025-09-04 16:55:45 +0100

    update import statement for logger in logBackendError module

--- src/api/get.js
+++ src/api/get.js
@@ -1,16 +1,14 @@
 import Wreck from '@hapi/wreck'
-import { config } from '../config/config.js'
-import { createLogger } from '../common/helpers/logging/logger.js'
-
-const logger = createLogger()
+import { buildBackendUrl } from '../common/utils/build-backend-url.js'
+import { logBackendError } from '../common/utils/log-backend-error.js'
 
 export async function get (url) {
-  const backendUrl = `${config.get('backend.endpoint')}${config.get('backend.path')}${url}`
+  const backendUrl = buildBackendUrl(url)
 
   try {
     return await Wreck.get(backendUrl)
   } catch (err) {
-    logger.error(`Encountered error while calling the backend with url: ${backendUrl}`, err)
+    logBackendError(backendUrl, err)
     throw err
   }
 }
--- src/api/post.js
+++ src/api/post.js
@@ -1,16 +1,14 @@
 import Wreck from '@hapi/wreck'
-import { config } from '../config/config.js'
-import { createLogger } from '../common/helpers/logging/logger.js'
-
-const logger = createLogger()
+import { buildBackendUrl } from '../common/utils/build-backend-url.js'
+import { logBackendError } from '../common/utils/log-backend-error.js'
 
 export async function post (url, payload) {
-  const backendUrl = `${config.get('backend.endpoint')}${config.get('backend.path')}${url}`
+  const backendUrl = buildBackendUrl(url)
 
   try {
     return await Wreck.post(backendUrl, { payload })
   } catch (err) {
-    logger.error(`Encountered error while calling the backend with url: ${url}`, err)
+    logBackendError(backendUrl, err)
     throw err
   }
 }
