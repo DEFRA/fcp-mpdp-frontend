fcaffc1d31c8178701f39ec7fa4cf716cd333156
Rana Salem <ranatasalem@gmail.com>
2025-09-12 08:15:41 +0100

    render search filters

--- test/utils/helpers.js
+++ test/utils/helpers.js
@@ -1,3 +1,5 @@
+import staticAmounts from '../../src/data/filters/amounts.js'
+
 export function getOptions (page, method = 'GET', params = {}, listParams = {}) {
   const urlParams = new URLSearchParams(params)
   for (const key in listParams) {
@@ -11,3 +13,25 @@ export function getOptions (page, method = 'GET', params = {}, listParams = {}) 
     url: `/${page}?${urlParams.toString()}`
   }
 }
+
+export function getMatchingStaticAmount (amounts) {
+  if (!amounts?.length) {
+    return []
+  }
+
+  const _amounts = amounts?.map(x => parseFloat(x))
+
+  const returnAmounts = staticAmounts.filter(range => {
+    const [from, to] = range.value.split('-')
+    return _amounts?.some(amount => {
+      if (!to) {
+        return amount > parseFloat(from)
+      }
+
+      return amount >= parseFloat(from) && amount <= parseFloat(to)
+    })
+  })
+
+  return returnAmounts
+}
+
