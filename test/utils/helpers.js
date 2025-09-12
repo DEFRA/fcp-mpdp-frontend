4707e431b649ef7b7d8c8b43f5731134d101ccf1
Rana Salem <ranatasalem@gmail.com>
2025-09-12 11:15:32 +0100

    remove getMatchingStaticAmounts module

--- test/utils/helpers.js
+++ test/utils/helpers.js
@@ -1,5 +1,3 @@
-import staticAmounts from '../../src/data/filters/amounts.js'
-
 export function getOptions (page, method = 'GET', params = {}, listParams = {}) {
   const urlParams = new URLSearchParams(params)
   for (const key in listParams) {
@@ -12,25 +10,4 @@ export function getOptions (page, method = 'GET', params = {}, listParams = {}) 
     method,
     url: `/${page}?${urlParams.toString()}`
   }
-}
-
-export function getMatchingStaticAmount (amounts) {
-  if (!amounts?.length) {
-    return []
-  }
-
-  const _amounts = amounts?.map(x => parseFloat(x))
-
-  const returnAmounts = staticAmounts.filter(range => {
-    const [from, to] = range.value.split('-')
-    return _amounts?.some(amount => {
-      if (!to) {
-        return amount > parseFloat(from)
-      }
-
-      return amount >= parseFloat(from) && amount <= parseFloat(to)
-    })
-  })
-
-  return returnAmounts
 }
