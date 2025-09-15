394f70a613eac4b6848ac033de489f6d795ea63f
Rana Salem <ranatasalem@gmail.com>
2025-09-15 16:39:49 +0100

    download search results

---
+++ src/routes/download/results.js
@@ -0,0 +1,42 @@
+import http2 from 'node:http2'
+import { post } from '../../api/post.js'
+import { resultsQuery as query } from '../../routes/queries/results.js'
+
+const { constants: httpConstants } = http2
+
+export const downloadResults = {
+  method: 'GET',
+  path: '/results/file',
+  options: {
+    validate: {
+      query,
+      failAction: async function (_request, h, error) {
+        return h
+          .response(error.toString())
+          .code(httpConstants.HTTP_STATUS_BAD_REQUEST)
+          .takeover()
+      }
+    },
+    handler: async function (request, h) {
+      const { searchString, schemes, amounts, counties, years } = request.query
+      const sortBy = decodeURIComponent(request.query.sortBy)
+
+      const filterBy = {
+        schemes: typeof schemes === 'string' ? [schemes] : schemes,
+        amounts: typeof amounts === 'string' ? [amounts] : amounts,
+        counties: typeof counties === 'string' ? [counties] : counties,
+        years: typeof years === 'string' ? [years] : years
+      }
+
+      const content = await post('/file', { searchString, filterBy, sortBy })
+
+      return h
+        .response(content?.payload)
+        .type('application/csv')
+        .header(
+          'Content-Disposition',
+          'attachment; filename="ffc-payment-results.csv"'
+        )
+    }
+  }
+}
