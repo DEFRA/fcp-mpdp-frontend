7421b33cd7901a26d17b2463269f8be79351dba4
Rana Salem <ranatasalem@gmail.com>
2025-10-06 16:39:33 +0100

    wip pt. 2

--- src/client/javascripts/application.js
+++ src/client/javascripts/application.js
@@ -1,4 +1,3 @@
-import { config } from '../../config/config.js'
 import { initAll } from 'govuk-frontend'
 import schemePaymentsByYear from './scheme-payments-by-year.js'
 import search from './search.js'
@@ -9,7 +8,6 @@ import searchFilterTags from './search-filter-tags.js'
 import details from './details.js'
 import print from './print.js'
 import cookies from './cookies.js'
-import googleTagManager from './google-tag-manager.js'
 
 schemePaymentsByYear.init()
 search.init()
@@ -20,6 +18,5 @@ searchFilterTags.init()
 details.init()
 print.init()
 cookies.init()
-googleTagManager.init(config.get('googleAnalytics.googleTagManagerKey'))
 
 initAll()
