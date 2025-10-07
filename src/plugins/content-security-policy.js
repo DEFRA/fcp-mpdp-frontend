d7561d32764e2251a09f9977cd5fdad06681c175
Rana Salem <ranatasalem@gmail.com>
2025-10-07 09:35:36 +0100

    update csp plugin

--- src/plugins/content-security-policy.js
+++ src/plugins/content-security-policy.js
@@ -3,17 +3,18 @@ import Blankie from 'blankie'
 // Hash 'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw=' is to support a GOV.UK frontend script bundled within Nunjucks macros
 // https://frontend.design-system.service.gov.uk/import-javascript/#if-our-inline-javascript-snippet-is-blocked-by-a-content-security-policy
 
-const gtmUrl = 'https://www.googletagmanager.com'
+const googleTagManagerUrl = 'https://*.googletagmanager.com'
+const googleAnalyticsUrl = 'https://*.google-analytics.com'
 
 export const contentSecurityPolicy = {
   plugin: Blankie,
   options: {
     fontSrc: ['self'],
-    imgSrc: ['self', gtmUrl, 'https://www.google-analytics.com'],
-    scriptSrc: ['self', "'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw='", gtmUrl],
+    imgSrc: ['self', googleTagManagerUrl, googleAnalyticsUrl],
+    scriptSrc: ['self', "'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw='", googleTagManagerUrl],
     styleSrc: ['self'],
-    connectSrc: ['self', gtmUrl, 'https://www.google-analytics.com'],
-    frameSrc: [gtmUrl],
+    connectSrc: ['self', googleTagManagerUrl, googleAnalyticsUrl, 'https://*.analytics.google.com'],
+    frameSrc: [googleTagManagerUrl],
     frameAncestors: ['self'],
     formAction: ['self'],
     manifestSrc: ['self'],
