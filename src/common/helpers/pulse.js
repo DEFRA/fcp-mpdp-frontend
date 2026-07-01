7a10e453a73a39a5eb71ddfdef46ca18b0e1e30d
John Watson <john.watson1@defra.gov.uk>
2026-07-02 00:20:41 +0100

    Reduce hapi-pulse shutdown timeout in development (#77)

--- src/common/helpers/pulse.js
+++ src/common/helpers/pulse.js
@@ -1,13 +1,15 @@
 import hapiPulse from 'hapi-pulse'
 import { createLogger } from './logging/logger.js'
+import { config } from '../../config/config.js'
 
 const tenSeconds = 10 * 1000
+const oneSecond = 1 * 1000
 
 const pulse = {
   plugin: hapiPulse,
   options: {
     logger: createLogger(),
-    timeout: tenSeconds
+    timeout: config.get('isDevelopment') ? oneSecond : tenSeconds
   }
 }
 
